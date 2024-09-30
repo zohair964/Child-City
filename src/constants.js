dc8b62234b7117adf68b36fa2542418d1f69b2c2
devaziz1 <157882617+devaziz1@users.noreply.github.com>
2024-09-30 17:33:18 +0500

    BACKEND URL Updated

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,3 @@
 // export const backendUrl = "http://localhost:6969";
-export const backendUrl =
-  "https://child-city-backend-a7000291e61e.herokuapp.com";
+export const backendUrl = "https://child-city-bk-c4c0c9e39c23.herokuapp.com";
 export const sizes = ["3-6M", "6-12M", "1-2Y", "2-3Y", "3-4Y"];
